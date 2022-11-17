11fe85b781039d77edb58b60c01500a07b0219dc
Sotorev <73616133+Sotorev@users.noreply.github.com>
2022-11-17 16:45:35 -0600

    New component for section titles

--- src/components/SectionTitle/index.js
+++ src/components/SectionTitle/index.js
@@ -1,8 +1,15 @@
 import './index.scss';
+import AnimattedLetters from '../AnimatedLetters'
 export default function SectionTitle({text}) {
 	return (
-		<h3>
-			{text}
+		<h3 className='section-title'>
+			<AnimattedLetters
+				letterClass={'blast'}
+				isLoaded={true}
+				strArray={Array.from(text)}
+				idx={0}
+			/>
+			
 		</h3>
 	);
 } 
