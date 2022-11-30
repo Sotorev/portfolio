5c1e9d409356b753fd2ae255a465439f3ec9c9ac
Sotorev <73616133+Sotorev@users.noreply.github.com>
2022-11-30 11:30:00 -0600

    ptool component enhanced

---
+++ src/assets/icons/styledcomponents.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from '../images/styled_components.png'
+const styledcomponents = () => {
+	return (
+		<img src={logo} alt="styled components" width={64}/>
+	)
+}
+
+export default styledcomponents
